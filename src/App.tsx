8936b0f1b2932a924c981837cb4200dfcc88d8df
Jalaj Garima <jalaj.garima@team.telstra.com>
2021-09-11 21:21:06 +1000

    base commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React, { FC } from 'react';
+
+import './App.css';
+
+const App: FC = () => {
+  return (
+    <div className="App">
+      <h1>Cell Simulator</h1>
+    </div>
+  );
+}
+
+export default App;
